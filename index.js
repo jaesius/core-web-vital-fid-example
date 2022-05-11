8ea3ab6aa815431679e516a804e0e59169a757dd
jgoddard <jgoddard@newrelic.com>
2022-05-11 14:40:10 -0700

    instrumentation for browser attempt

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const port = process.env.PORT || 3000;
  app.set("views", path.join(__dirname, "views"));
  app.set("view engine", "pug");
  app.use(express.static(path.join(__dirname, "public")));
+ app.locals.newrelic = newrelic;
 
 /**
  * Routes Definitions
